e9fb4ed7bc03733a8b86aaf85beacce9e086c02e
jozue <jozue@DESKTOP-ORD4RU3>
2021-10-13 10:24:13 -0500

    Segunda version mas hola

--- src/main/java/com/mycompany/githubprueba1/Main.java
+++ src/main/java/com/mycompany/githubprueba1/Main.java
@@ -12,6 +12,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundos");
                 System.out.println("Hola mundo");
+                                System.out.println("Holaaa mundo");
+
+                
 
     }
 }
